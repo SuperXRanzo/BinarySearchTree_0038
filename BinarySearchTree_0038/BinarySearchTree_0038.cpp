35fe3720ed6749cd40659f70844fddbf9750fc49
SuperXRanzo <okanskyavenger@gmail.com>
2024-06-13 18:37:00 +0700

    adding green text

--- BinarySearchTree_0038/BinarySearchTree_0038.cpp
+++ BinarySearchTree_0038/BinarySearchTree_0038.cpp
@@ -9,6 +9,7 @@ public:
 	Node* leftchild;
 	Node* rightchild;
 
+	//costructor for the node class
 	Node(string i, Node* l, Node* r)
 	{
 		info = i;
@@ -24,15 +25,15 @@ public:
 
 	BinaryTree() 
 	{
-		ROOT = nullptr; 
+		ROOT = nullptr; //initializing ROOT to null
 	}
 
-	void insert(string element) 
+	void insert(string element) //insert a node in the binary search tree
 	{
-		Node* newNode = new Node(element, nullptr, nullptr);
-		newNode->info = element;
-		newNode->leftchild = nullptr;
-		newNode->rightchild = nullptr;
+		Node* newNode = new Node(element, nullptr, nullptr);//Allocate memory for the new node
+		newNode->info = element;//assign value to data field of the new node
+		newNode->leftchild = nullptr;//make the left child of the new node point to null
+		newNode->rightchild = nullptr;//make the right child of the new node point to null
 
 		Node* parent = nullptr;
 		Node* currentNode = nullptr;
@@ -40,8 +41,8 @@ public:
 
 		if (parent == nullptr) 
 		{
-			ROOT = newNode;
-			return;
+			ROOT = newNode; // mark the new node as  root
+			return; // exit
 		}
 
 		if (element < parent->info) 
@@ -53,11 +54,13 @@ public:
 			parent->rightchild = newNode;
 		}
 	}
-	
-	void search(string element, Node*& parent, Node*& currentNode) {
+
+	void search(string element, Node*& parent, Node*& currentNode) 
+	{
 		currentNode = ROOT;
 		parent = nullptr;
-		while ((currentNode != nullptr) && (currentNode->info != element)) {
+		while ((currentNode != nullptr) && (currentNode->info != element)) 
+		{
 			parent = currentNode;
 			if (element < currentNode->info)
 				currentNode = currentNode->leftchild;
@@ -66,7 +69,8 @@ public:
 		}
 	}
 
-	void inorder(Node* ptr) {
+	void inorder(Node* ptr) 
+	{
 		if (ROOT == nullptr)
 		{
 			cout << "Tree is empty" << endl;
@@ -80,8 +84,10 @@ public:
 		}
 	}
 
-	void preorder(Node* ptr) {
-		if (ROOT == nullptr) {
+	void preorder(Node* ptr) 
+	{
+		if (ROOT == nullptr) 
+		{
 			cout << "Tree Is Empty" << endl;
 			return;
 		}
@@ -93,8 +99,10 @@ public:
 		}
 	}
 
-	void postorder(Node* ptr) {
-		if (ROOT == nullptr) {
+	void postorder(Node* ptr) 
+	{
+		if (ROOT == nullptr) 
+		{
 			cout << "Tree Is Empty" << endl;
 			return;
 		}
@@ -107,7 +115,8 @@ public:
 	}
 };
 
-int main() {
+int main() 
+{
 	BinaryTree x;
 	while (true)
 	{
